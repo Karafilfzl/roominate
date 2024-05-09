fcd4e5cfcdf7e1bf50f81330195755136575b173
Karafil Fazli <karafil.fazli@hes-so.ch>
2024-05-09 18:51:50 +0200

    reset project

--- roominate/routes/api.php
+++
@@ -1,7 +0,0 @@
-Route::apiResource('users', 'Api\UserController');
-Route::apiResource('buildings', 'Api\BuildingController');
-Route::apiResource('rooms', 'Api\RoomController');
-Route::apiResource('reservations', 'Api\ReservationController');
-Route::apiResource('courses', 'Api\CourseController');
-Route::apiResource('timeslots', 'Api\TimeslotController');
-Route::apiResource('events', 'Api\EventController');
